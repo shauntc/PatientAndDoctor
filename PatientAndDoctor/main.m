8b141196bc9c90d3e38758affea028fb130e41e7
shauntc <shaun@shauntc.com>
2016-09-08 11:53:15 -0700

    Initial Commit

---
+++ PatientAndDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PatientAndDoctor
+//
+//  Created by Shaun Campbell on 2016-09-08.
+//  Copyright © 2016 Shaun Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
